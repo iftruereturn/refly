2622c267c49e7e291ff587092c162bb8fb328990
iftruereturn <qwertyzxc11@gmail.com>
2017-01-09 21:41:07 +0300

    Add AuthContainer outline

---
+++ frontend/js/containers/AuthContainer.jsx
@@ -0,0 +1,31 @@
+import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
+
+import userActions from '../actions/user';
+
+class AuthContainer extends Component {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    username: PropTypes.string.isRequired,
+    isAuthenticated: PropTypes.bool.isRequired,
+  }
+
+  render() {
+    const { children,
+      username, isAuthenticated,
+      signup, login } = this.props;
+
+    return (
+      <div></div>
+    );
+  }
+}
+
+const mapStateToProps = state => (
+  {
+    ...state.user,
+  }
+);
+
+export default connect(mapStateToProps, userActions)(AuthContainer);
